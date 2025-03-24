0bc9e44bc6abed8dba34572d1e628fbfd4b5155b
Arthur Valiev <xylophone909@gmail.com>
2025-03-24 21:16:03 +0300

    Добавил метод подсчёта калорий за день

--- src/main/java/org/example/systems1221/Food.java
+++ src/main/java/org/example/systems1221/Food.java
@@ -5,7 +5,7 @@ import java.sql.*;
 
 
 public class Food {
-    public void addFood(int id, String name, int calories, double proteins, double fats, double carbohydrates) {
+    public void addFoodIntoList(int id, String name, int calories, double proteins, double fats, double carbohydrates) {
         String url = "jdbc:postgresql://localhost:5432/Weight_loss";
         String user = "Arthur";
         String password = "123";
@@ -47,6 +47,67 @@ public class Food {
                 System.out.println("Количество протеинов на 100гр: " + proteins);
                 System.out.println("Количество жиров на 100гр: " + fats);
                 System.out.println("Количество углеводов на 100гр: " + carbohydrates);
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void addMeal(int id, Date date, String day, String meal) {
+        String url = "jdbc:postgresql://localhost:5432/Weight_loss";
+        String user = "Arthur";
+        String password = "123";
+        String sql = "INSERT INTO days_of_week VALUES (?,?,?,?)";
+        try {
+            Connection connection = DriverManager.getConnection(url, user, password);
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, id);
+            preparedStatement.setDate(2, date);
+            preparedStatement.setString(3, day);
+            preparedStatement.setString(4, meal);
+            preparedStatement.executeUpdate();
+            System.out.println("Приём Пищи добавлен!");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void caloriesOfTheDay(Date date) {
+        String url = "jdbc:postgresql://localhost:5432/Weight_loss";
+        String user = "Arthur";
+        String password = "123";
+        String sql = "SELECT "
+                + "d.date, "
+                + "d.day, "
+                + "COUNT(d.meal) AS meals_count, "
+                + "SUM(f.calories_on_portion) AS total_calories, "
+                + "SUM(f.proteins) AS total_proteins, "
+                + "SUM(f.fats) AS total_fats, "
+                + "SUM(f.carbohydrates) AS total_carbohydrates , "
+                + "STRING_AGG(d.meal, ', ') AS meals_list "
+                + "FROM days_of_week d "
+                + "JOIN food f ON d.meal = f.name "
+                + "WHERE d.date = ? "
+                + "GROUP BY d.date, d.day";
+
+        try (Connection connection = DriverManager.getConnection(url, user, password);
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setDate(1, new Date(date.getTime()));
+
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    String meals = resultSet.getString("meals_list");
+                    int calories = resultSet.getInt("total_calories");
+
+                    System.out.println("Съеденное за " + resultSet.getDate("date") + ":");
+                    System.out.println("Блюда: " + meals);
+                    System.out.println("Общее количество калорий: " + calories);
+                    System.out.println("Белки: " + resultSet.getDouble("total_proteins") + "г");
+                    System.out.println("Жиры: " + resultSet.getDouble("total_fats") + "г");
+                    System.out.println("Углеводы: " + resultSet.getDouble("total_carbohydrates") + "г");
+                } else {
+                    System.out.println("Нет данных за указанную дату");
+                }
             }
         } catch (SQLException e) {
             System.out.println(e.getMessage());
--- src/main/java/org/example/systems1221/Systems1221Application.java
+++ src/main/java/org/example/systems1221/Systems1221Application.java
@@ -3,12 +3,14 @@ package org.example.systems1221;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.Date;
+
 @SpringBootApplication
 public class Systems1221Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Systems1221Application.class, args);
         Food food = new Food();
-        food.listOfDishes(1);
+        food.caloriesOfTheDay(Date.valueOf("2025-03-24"));
     }
 }
